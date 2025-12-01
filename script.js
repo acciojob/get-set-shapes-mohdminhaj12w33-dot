c5d806b70e69a711bc1c3f9e43fcb804e395e8fe
mohdminhaj12w33-dot <mohdminhaj12w33@gmail.com>
2025-12-01 21:39:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+  constructor(width, height) {
+    if (width <= 0 || height <= 0) {
+      throw new Error("Width and height must be positive integers.");
+    }
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  get width() {
+    return this._width;
+  }
 
-// Do not change the code below this line
+  get height() {
+    return this._height;
+  }
+
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side) {
+    if (side <= 0) {
+      throw new Error("Side must be a positive integer.");
+    }
+    super(side, side); // Rectangle constructor sets width & height
+    this._side = side;
+  }
+
+  getPerimeter() {
+    return this._side * 4;
+  }
+}
+
+// Make them available for Cypress tests
 window.Rectangle = Rectangle;
 window.Square = Square;
+
+
+
+// Do not change the code below this 
